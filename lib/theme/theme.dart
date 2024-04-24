2090c48d7c5e8bc0ec30bcb0edd47eb8bc8151a2
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-04-24 15:47:26 +0530

    add theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
+    useMaterial3: true,
+    scaffoldBackgroundColor: const Color.fromRGBO(21, 18, 19, 1.0),
+    fontFamily: 'Inter',
+    textTheme: const TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 32.0,
+        fontWeight: FontWeight.w900,
+        color: Colors.white,
+        letterSpacing: -2,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 14.0,
+        letterSpacing: -0.25,
+      ),
+    ),
+  );
+}
