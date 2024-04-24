f1c9774b7046bc93fe91d964558f8362d0e1988e
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-04-24 15:53:41 +0530

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:simply_qibla/pages/map_page.dart';
+import 'package:simply_qibla/theme/theme.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     statusBarColor: Colors.transparent,
     systemNavigationBarColor: Colors.transparent,
   ));
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -20,22 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simply Qibla',
       themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
-        useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromRGBO(21, 18, 19, 1.0),
-        fontFamily: 'Inter',
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-          titleLarge: TextStyle(
-            fontSize: 32.0,
-            fontWeight: FontWeight.w900,
-            color: Colors.white,
-            letterSpacing: -2,
-          ),
-          bodyMedium: TextStyle(fontSize: 14.0),
-        ),
-      ),
+      darkTheme: AppThemes.darkTheme,
       home: const MapPage(),
     );
   }
