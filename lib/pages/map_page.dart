d5fee27f3133732f3963adc79186d0945b4bffc8
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-05-08 16:08:15 +0530

    replace address search bar with coordinates input

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -3,8 +3,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:simply_qibla/widgets/coordinates_form_bar.dart';
 import 'package:simply_qibla/widgets/sq_app_bar.dart';
-import 'package:simply_qibla/widgets/address_search_bar.dart';
 import 'package:simply_qibla/constants/constants.dart';
 import 'package:simply_qibla/styles/style.dart';
 
