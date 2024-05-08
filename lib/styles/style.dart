85aaf576335bbc2d7df42b1e9bc3a941cbbc5350
ahmd-sh <ahmed.shaikh626@gmail.com>
2024-05-08 16:05:46 +0530

    add size for border radius sm and bar

--- lib/styles/style.dart
+++ lib/styles/style.dart
@@ -6,10 +6,12 @@ class AppPadding {
 
 class AppDimensions {
   static const double appBarPreferredSize = 80.0;
+  static const double pillBarSize = 50.0;
   static const double iconSizeSm = 18.0;
   static const double iconSizeMd = 26.0;
   static const double iconSizeLg = 32.0;
   static const double iconSizeXl = 50.0;
+  static const double borderRadiusSm = 8.0;
   static const double borderRadiusMd = 12.0;
   static const double borderRadiusLg = 16.0;
   static const double borderRadiusXl = 28.0;
